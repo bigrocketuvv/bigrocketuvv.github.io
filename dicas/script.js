e330aaadb938e79e620dbe0ed546753950796db8
bigrocketuvv <50712590+bigrocketuvv@users.noreply.github.com>
2019-06-02 18:37:54 -0300

    pagina Dicas

---
+++ dicas/script.js
@@ -0,0 +1,9 @@
+(function() {
+    ("#links > a").click(function(e) {
+        e.preventDefault(); //so the browser doesn't follow the link
+
+        ("#content").load(this.href, function() {
+            //execute here after load completed
+        });
+    });
+});
